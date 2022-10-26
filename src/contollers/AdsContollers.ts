055152dd95c53ea1c1269b38aa31d1d7dbf2bb04
Michael da silva ribeiro <55338900+MichaelWEB3@users.noreply.github.com>
2022-10-25 22:08:03 -0300

    .

--- src/contollers/AdsContollers.ts
+++ src/contollers/AdsContollers.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
+import { json, Request, Response } from "express";
 import Categories from "../models/Categories";
 import * as dotenv from "dotenv";
 import Ads from "../models/Ads";
 import Users from "../models/Users";
 import { v4 as uuidv4 } from "uuid";
 import jimp from "jimp";
+import States from "../models/States";
 
 const addimg = async (buffer: any) => {
   let newName = `${uuidv4()}.jpg`;
@@ -61,6 +62,58 @@ export const setAd = async (req: Request, resp: Response) => {
 
   resp.json({ satus: "sucess", users });
 };
-export const getList = async (req: Request, resp: Response) => {};
+export const getList = async (req: Request, resp: Response) => {
+  const { sort = "asc", offset = 0, limit = 8, q, cat, state } = req.query;
+  console.log(q);
+  let filtes: any = { status: true };
+  if (q) {
+    console.log(q);
+    filtes.title = { $regex: q, $options: "i" };
+  }
+  if (cat) {
+    const c = await Categories.findOne({ slug: cat }).exec();
+    if (c) filtes.category = c._id.toHexString();
+  }
+  if (state) {
+    const s = await States.findOne({ name: state }).exec();
+    if (s) filtes.state = s._id.toHexString();
+  }
+
+  const adsTotal = await Ads.find(filtes).exec();
+  const total = adsTotal?.length;
+  const adsDate = await Ads.find(filtes)
+    .sort({
+      dateCreated: sort == "desc" ? -1 : 1,
+    })
+    .skip(Number(offset))
+    .limit(Number(limit))
+    .exec();
+  let ads = [];
+
+  for (let i in adsDate) {
+    let image;
+
+    let defaltImg = adsDate[i].imgs.find((e) => e.defalt == true);
+    if (defaltImg) {
+      image = `${process.env.BASE}/media/${defaltImg.url}`;
+    } else {
+      image = `${process.env.BASE}/media/defalut.jpg`;
+    }
+    ads.push({
+      id: adsDate[i]._id,
+      title: adsDate[i]?.title,
+      price: adsDate[i]?.price,
+      priceNegocible: adsDate[i]?.priceNegocible,
+      image,
+    });
+  }
+
+  resp.json({ status: "sucess", ads, total });
+};
 export const editAd = async (req: Request, resp: Response) => {};
-export const getItem = async (req: Request, resp: Response) => {};
+export const getItem = async (req: Request, resp: Response) => {
+  const idPost = req.query.id;
+  const getOneAd = await Ads.findById(idPost);
+  console.log(getOneAd);
+  resp.json({ status: "success", getOneAd });
+};
